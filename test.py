22216beeaa55d0bc7a09324f38d005bf72e63edf
EinoHR <EinoHR@noreply.codeberg.org>
2021-08-21 19:14:39 +0300

    Installing, starting, and playing from the JSON API

--- test.py
+++ test.py
@@ -1,26 +1,38 @@
 import requests
 import subprocess
 import zipfile
+import os
+import stat
+
+# Checks if LBRYNET is installed
+# If not, it calls InstalLBRYNet, if it is it starts it
+def StartLBRYNet():
+    try:
+        with open("lbrynet.log", "a") as log:
+            lbrynet = subprocess.Popen(["lbrynet", "start"], stdout=log, stderr=log)
+    except:
+        InstalLBRYNet()
+
+# Installs latest build of LBRYNET and moves it to the appropriete directory.
+def InstalLBRYNet():
+    lbrynetdl = requests.get("https://github.com/lbryio/lbry-sdk/releases/latest/download/lbrynet-mac.zip")
+    open("lbrynet.zip","wb").write(lbrynetdl.content)
+    zip = zipfile.ZipFile("lbrynet.zip")
+    zip.extractall(path="/usr/local/bin/")
+    os.remove("lbrynet.zip")
+    execstat = os.stat("/usr/local/bin/lbrynet")
+    os.chmod("/usr/local/bin/lbrynet", execstat.st_mode | stat.S_IEXEC)
+    StartLBRYNet()
+
 
 # Start lbrynet if not running
 try:
     requests.post("http://localhost:5279")
 except:
-    startlbrynet()
+    StartLBRYNet()
 
-def startLBRYnet():
-    try:
-        with open("lbrynet.log", "a") as log:
-            lbrynet = subprocess.Popen(["lbrynet", "start"], stdout=log, stderr=log)
-    except:
-        instalLBRYnet()
 
-def instalLBRYnet():
-    lbrynetdl = requests.get("https://github.com/lbryio/lbry-sdk/releases/latest/download/lbrynet-mac.zip")
-    open("lbrynet.zip","wb").write(lbrynetdl.content)
-    lbrynetzip = ZipFile("lbrynet.zip")
-    lbrynetzip.extractall(path="/usr/local/bin/")
-
+# Uses LBRYNet to get 
 def playContent(uri):
     request = requests.post("http://localhost:5279", json={"method": "get", "params": {"uri": uri}}).json()
     if request.get("result").get("streaming_url") == None:
